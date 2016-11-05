96a5609c2754ac783782c93031587e516f1f29c0
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2016-11-05 17:07:55 +0100

    Issue #2823883 by legolasbo: Refactor AccessTest.php

--- src/Tests/AccessTest.php
+++ src/Tests/AccessTest.php
@@ -7,26 +7,22 @@ use Drupal\Core\Url;
 /**
  * Tests eck's access control.
  *
- * @group eck
+ * @group Eck
  *
  * @codeCoverageIgnore because we don't have to test the tests
  */
 class AccessTest extends TestBase {
 
-  /**
-   * @var
-   */
-  protected $entityType;
+  /** @var array */
+  protected $entityTypeInfo;
 
-  /**
-   * @var
-   */
-  protected $bundle;
+  /** @var array */
+  protected $bundleInfo;
 
   public function setUp() {
     parent::setUp();
-    $this->entityType = $this->createEntityType();
-    $this->bundle = $this->createEntityBundle($this->entityType['id']);
+    $this->entityTypeInfo = $this->createEntityType();
+    $this->bundleInfo = $this->createEntityBundle($this->entityTypeInfo['id']);
     // Start testing with a logged out user.
     $this->drupalLogout();
   }
@@ -42,14 +38,14 @@ class AccessTest extends TestBase {
         'entity.eck_entity_type.edit_form',
         'entity.eck_entity_type.delete_form',
       ],
-      "create {$this->entityType['id']} entities" => [
+      "create {$this->entityTypeInfo['id']} entities" => [
         'eck.entity.add_page',
         'eck.entity.add',
       ],
     ];
     $route_args = [
-      'eck_entity_type' => $this->entityType['id'],
-      'eck_entity_bundle' => $this->bundle['type'],
+      'eck_entity_type' => $this->entityTypeInfo['id'],
+      'eck_entity_bundle' => $this->bundleInfo['type'],
     ];
     foreach ($routes as $route_names) {
       foreach ($route_names as $route) {
@@ -73,39 +69,39 @@ class AccessTest extends TestBase {
    */
   public function testDynamicRoutes() {
     $routes = [
-      "view own {$this->entityType['id']} entities" => [
-        "eck.entity.{$this->entityType['id']}.list",
+      "view own {$this->entityTypeInfo['id']} entities" => [
+        "eck.entity.{$this->entityTypeInfo['id']}.list",
       ],
-      "view any {$this->entityType['id']} entities" => [
-        "eck.entity.{$this->entityType['id']}.list",
+      "view any {$this->entityTypeInfo['id']} entities" => [
+        "eck.entity.{$this->entityTypeInfo['id']}.list",
       ],
       'bypass eck entity access' => [
-        "eck.entity.{$this->entityType['id']}.list",
+        "eck.entity.{$this->entityTypeInfo['id']}.list",
       ],
       'administer eck entity bundles' => [
-        "eck.entity.{$this->entityType['id']}_type.list",
-        "eck.entity.{$this->entityType['id']}_type.add",
-        "entity.{$this->entityType['id']}_type.edit_form",
-        "entity.{$this->entityType['id']}_type.delete_form",
+        "eck.entity.{$this->entityTypeInfo['id']}_type.list",
+        "eck.entity.{$this->entityTypeInfo['id']}_type.add",
+        "entity.{$this->entityTypeInfo['id']}_type.edit_form",
+        "entity.{$this->entityTypeInfo['id']}_type.delete_form",
       ],
     ];
-    $route_args = [
-      "{$this->entityType['id']}_type" => $this->bundle['type'],
+    $routeArguments = [
+      "{$this->entityTypeInfo['id']}_type" => $this->bundleInfo['type'],
     ];
 
-    foreach ($routes as $route_names) {
-      foreach ($route_names as $route) {
-        $this->drupalGet(Url::fromRoute($route, $route_args));
-        $this->assertResponse(403, t('Anonymous users cannot access %route', ['%route' => $route]));
+    foreach ($routes as $routeNames) {
+      foreach ($routeNames as $routeName) {
+        $this->drupalGet(Url::fromRoute($routeName, $routeArguments));
+        $this->assertResponse(403, t('Anonymous users cannot access %route', ['%route' => $routeName]));
       }
     }
 
     \Drupal::entityTypeManager()->clearCachedDefinitions();
-    foreach ($routes as $permission => $route_names) {
+    foreach ($routes as $permission => $routeNames) {
       $this->drupalLogin($this->drupalCreateUser([$permission]));
-      foreach ($route_names as $route) {
-        $this->drupalGet(Url::fromRoute($route, $route_args));
-        $this->assertResponse(200, t('Users with the %perm permission can access %route', ['%route' => $route, '%perm' => $permission]));
+      foreach ($routeNames as $routeName) {
+        $this->drupalGet(Url::fromRoute($routeName, $routeArguments));
+        $this->assertResponse(200, t('Users with the %perm permission can access %route', ['%route' => $routeName, '%perm' => $permission]));
       }
     }
   }
@@ -114,59 +110,64 @@ class AccessTest extends TestBase {
    * Tests if access handling for created entities is handled correctly.
    */
   public function testEntityAccess() {
-    $own_permissions = $any_permissions = ["create {$this->entityType['id']} entities"];
+    $entityTypeName = $this->entityTypeInfo['id'];
+    $ownEntityPermissions = $anyEntityPermissions = ["create {$entityTypeName} entities"];
     foreach (['view', 'edit', 'delete'] as $op) {
-      $own_permissions[] = "{$op} own {$this->entityType['id']} entities";
-      $any_permissions[] = "{$op} any {$this->entityType['id']} entities";
+      $ownEntityPermissions[] = "{$op} own {$entityTypeName} entities";
+      $anyEntityPermissions[] = "{$op} any {$entityTypeName} entities";
     }
-    $ownEntityUser = $this->drupalCreateUser($own_permissions);
-    $anyEntityUser = $this->drupalCreateUser($any_permissions);
+    $ownEntityUser = $this->drupalCreateUser($ownEntityPermissions);
+    $anyEntityUser = $this->drupalCreateUser($anyEntityPermissions);
 
     $this->drupalLogin($anyEntityUser);
     $edit['title[0][value]'] = $this->randomMachineName();
     $route_args = [
-      'eck_entity_type' => $this->entityType['id'],
-      'eck_entity_bundle' =>  $this->bundle['type'],
+      'eck_entity_type' => $entityTypeName,
+      'eck_entity_bundle' =>  $this->bundleInfo['type'],
     ];
     $this->drupalPostForm(Url::fromRoute("eck.entity.add", $route_args), $edit, t('Save'));
 
     $this->drupalLogin($ownEntityUser);
     $edit['title[0][value]'] = $this->randomMachineName();
     $route_args = [
-      'eck_entity_type' => $this->entityType['id'],
-      'eck_entity_bundle' =>  $this->bundle['type'],
+      'eck_entity_type' => $entityTypeName,
+      'eck_entity_bundle' =>  $this->bundleInfo['type'],
     ];
     $this->drupalPostForm(Url::fromRoute("eck.entity.add", $route_args), $edit, t('Save'));
 
     // Get the entity that was created by the 'any' user.
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/1");
+    $arguments = [$entityTypeName => 1];
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.canonical", $arguments));
     $this->assertResponse(403, 'The user cannot see content which is not his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/1/edit");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.edit_form", $arguments));
     $this->assertResponse(403, 'The user cannot edit content which is not his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/1/delete");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.delete_form", $arguments));
     $this->assertResponse(403, 'The user cannot delete content which is not his own.');
     // Get the entity that was created by the 'own' user.
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/2");
+    $arguments = [$entityTypeName => 2];
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.canonical", $arguments));
     $this->assertResponse(200, 'The user can see content which is his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/2/edit");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.edit_form", $arguments));
     $this->assertResponse(200, 'The user can edit content which is his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/2/delete");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.delete_form", $arguments));
     $this->assertResponse(200, 'The user can delete content which not his own.');
 
     $this->drupalLogin($anyEntityUser);
     // Get the entity that was created by the 'any' user.
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/1");
+    $arguments = [$entityTypeName => 1];
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.canonical", $arguments));
     $this->assertResponse(200, 'The user can see content which is his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/1/edit");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.edit_form", $arguments));
     $this->assertResponse(200, 'The user can edit content which is his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/1/delete");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.delete_form", $arguments));
     $this->assertResponse(200, 'The user can delete content which is his own.');
     // Get the entity that was created by the 'own' user.
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/2");
+    $arguments = [$entityTypeName => 2];
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.canonical", $arguments));
     $this->assertResponse(200, 'The user can see content which is not his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/2/edit");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.edit_form", $arguments));
     $this->assertResponse(200, 'The user can edit content which is not his own.');
-    $this->drupalGet("admin/structure/eck/entity/{$this->entityType['id']}/2/delete");
+    $this->drupalGet(Url::fromRoute("entity.{$entityTypeName}.delete_form", $arguments));
     $this->assertResponse(200, 'The user can delete content which is not his own.');
   }
 
