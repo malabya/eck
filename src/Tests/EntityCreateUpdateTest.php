d38594bb504592d63faf28bc9f58d4bf84d8be52
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2016-11-05 18:47:13 +0100

    Issue #2825122 by legolasbo: Refactor EntityCreateUpdateTest.php

--- src/Tests/EntityCreateUpdateTest.php
+++ src/Tests/EntityCreateUpdateTest.php
@@ -2,34 +2,43 @@
 
 namespace Drupal\eck\Tests;
 
+use Drupal\Core\Url;
+
 /**
  * Tests if eck entities are correctly created and updated
  *
- * @group eck
+ * @group Eck
  *
  * @codeCoverageIgnore because we don't have to test the tests
  */
 class EntityCreateUpdateTest extends TestBase {
 
-  public function testIfEntityCreationDoesNotResultInMismatchedEntityDefinitions() {
-    $this->drupalLogin($this->drupalCreateUser([],[], TRUE));
+  public function setUp() {
+    parent::setUp();
+
+    $this->drupalLogin($this->drupalCreateUser([], [], TRUE));
+  }
+
+  public function testEntityCreationDoesNotResultInMismatchedEntityDefinitions() {
     $this->createEntityType([], 'TestType');
 
-    $this->drupalGet('admin/reports/status');
-
-    $this->assertNoRaw('Mismatched entity and/or field definitions');
+    $this->assertNoMismatchedFieldDefinitions();
   }
 
   public function testIfEntityUpdateDoesNotResultInMismatchedEntityDefinitions() {
-    $this->testIfEntityCreationDoesNotResultInMismatchedEntityDefinitions();
-    $path = 'admin/structure/eck/entity_type/manage/testtype';
-    $edit = [
-      'created' => FALSE
-    ];
+    $this->createEntityType([], 'TestType');
 
-    $this->drupalPostForm($path, $edit, t('Update @type', ['@type' => 'TestType']));
-    $this->drupalGet('admin/reports/status');
+    $routeArguments = ['eck_entity_type' => 'testtype'];
+    $route = 'entity.eck_entity_type.edit_form';
+    $edit = ['created' => FALSE];
+    $submitButton = t('Update @type', ['@type' => 'TestType']);
+    $this->drupalPostForm(Url::fromRoute($route, $routeArguments), $edit, $submitButton);
 
+    $this->assertNoMismatchedFieldDefinitions();
+  }
+
+  public function assertNoMismatchedFieldDefinitions() {
+    $this->drupalGet(Url::fromRoute('system.status'));
     $this->assertNoRaw('Mismatched entity and/or field definitions');
   }
 
